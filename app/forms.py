4d9bfd4861297bc4c9911565ce0a0272595d25f8
Matt Robinson <mwr54@nbcs.rutgers.edu>
2013-07-01 14:30:09 -0400

    Some refactoring in preparation for deployment

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form, TextField, SelectField
-from flask.ext.wtf import Required
-
-#form data for the dropdown menu and searchbar
-class SearchForm(Form):
-    function_name = TextField('function_name', validators = [Required()])
-    searchby = SelectField('searchby', choices = [('name', 'Name'), ('file', 'File'), ('provides', 'Provides'), ('requires', 'Requires'), ('description', 'Description'), ('summary', 'Summary')])
