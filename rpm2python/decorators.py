45858ae30d18cce92bb44ee94c9ef45d5b2b2ee4
Matt Robinson <mrobinson7627@gmail.com>
2013-08-09 14:00:31 -0400

    Started adding doc strings

--- rpm2python/decorators.py
+++ rpm2python/decorators.py
@@ -1,6 +1,7 @@
 from threading import Thread
 
 def async(f):
+    """Make a function asynchronous"""
     def wrapper(*args, **kwargs):
         thr = Thread(target = f, args = args, kwargs = kwargs)
         thr.start()
--- rpm2python/forms.py
+++ rpm2python/forms.py
@@ -3,6 +3,7 @@ from flask.ext.wtf import Required
 
 #form data for the dropdown menu and searchbar
 class SearchForm(Form):
+    """Provides the search form at the top of every page"""
     function_name = TextField('function_name', validators = [Required()])
     searchby = SelectField('searchby', choices = [
                                             ('name', 'Name'),
