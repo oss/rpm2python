6eef1b0d07bf06a4866dc0c6e3c10afa80b7eaa7
Alexander Pavel <alexpavel123@gmail.com>
2017-04-25 15:51:39 -0400

    Temporary fix for CentOS 7 (this will at least get populate-rpmfind-db working...)

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/heroes/u1/anp120/git/rpm2python/venv"
+set -gx VIRTUAL_ENV "/var/www/rpm2python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
